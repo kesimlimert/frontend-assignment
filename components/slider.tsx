109c374e787f4a3b273edd1855af165d03433982
kesimlimert <mert_cod@hotmail.com>
2024-10-20 03:51:24 +0300

    import image name fix

--- components/slider.tsx
+++ components/slider.tsx
@@ -4,7 +4,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Image from "next/image";
-import Quote from "@/public/quote.svg";
+import Quote from "@/public/Quote.svg";
 
 interface Reference {
   body: string;
